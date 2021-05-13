6a9abfffae2b4a65bb1b92941fb92697c85c3794
Daniel Gutierrez <43121233+dgutierrezd@users.noreply.github.com>
2021-05-13 00:52:10 +0000

    change host to codespaces GH

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,5 +63,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << "fa23dcecb158.ngrok.io"
+  # config.hosts << "fa23dcecb158.ngrok.io"
+  config.hosts << "dgutierrezd-rails-api-7pq7-3000.githubpreview.dev"
 end
